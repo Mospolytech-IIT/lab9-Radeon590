9b215a9574ead432fdd10ef5e2106f6256a7fb9d
Radeon590 <59920979+Radeon590@users.noreply.github.com>
2025-01-13 15:29:33 +0300

    db connection and tables creation

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import init_models, User, Post
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    # Настройки подключения
+    DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/pysqlalchemy"
+    engine = create_engine(DATABASE_URL, echo=True)
+    SessionLocal = sessionmaker(bind=engine)
+    init_models(engine)
+    #session = SessionLocal()
+    print("Таблицы успешно созданы!")
